76bb48e812524ddfd2462bc445e9d972d44540e6
2022ict38 <dilmiapsara151@gmail.com>
2026-05-15 15:29:43 +0530

    Implemented the fetch method in the taskController.js file

--- taskController.js
+++ taskController.js
@@ -19,3 +19,15 @@ export const create = async (req, res) => {
   }
 };
 
+// READ: Fetch all available tasks from the database
+export const fetch = async (req, res) => {
+  try {
+    const tasks = await Task.find();
+    if (tasks.length === 0) {
+      return res.status(404).json({ message: "No tasks or deadlines found." });
+    }
+    return res.status(200).json(tasks);
+  } catch (error) {
+    return res.status(500).json({ error: "Internal Server Error." });
+  }
+};
